150a21470203c5cf0ab7247a71bccc94339dded0
Aidai Almaz kyzy <aalmazkyzy@gmail.com>
2022-04-29 21:01:41 +0600

    Edited the template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'stopwatch App';
+  title = 'stopwatch';
   subtitle: string = 'Stopwatch built on Angular';
   minutes = 0
   seconds = 0;
@@ -16,30 +17,25 @@ export class AppComponent {
   onStartTimer() {
     this.timerID = setInterval(() => {
       this.tens++;
-
       if (this.tens >= 100) {
         this.seconds++;
         this.tens = 0;
       }
-
       if (this.seconds >= 60) {
         this.minutes++;
         this.seconds = 0
       }
     });
   }
-
   onStopTimer() {
     clearInterval(this.timerID);
   }
-
   onResetTimer() {
     clearInterval(this.timerID);
     this.minutes = 0;
     this.seconds = 0;
     this.tens = 0;
   }
-
   format(num: number) {
     return (num + '').length === 1 ? '0' + num : num + '';
   }
