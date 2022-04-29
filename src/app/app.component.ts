6c5df20b26eedf7f84990a678c81b558ab517dcf
Aidai Almaz kyzy <aalmazkyzy@gmail.com>
2022-04-29 20:52:00 +0600

    Completed template, css, ts files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'stopwatchApp';
+  title = 'stopwatch App';
+  subtitle: string = 'Stopwatch built on Angular';
+  minutes = 0
+  seconds = 0;
+  tens = 0;
+  timerID = 0;
+
+  onStartTimer() {
+    this.timerID = setInterval(() => {
+      this.tens++;
+
+      if (this.tens >= 100) {
+        this.seconds++;
+        this.tens = 0;
+      }
+
+      if (this.seconds >= 60) {
+        this.minutes++;
+        this.seconds = 0
+      }
+    });
+  }
+
+  onStopTimer() {
+    clearInterval(this.timerID);
+  }
+
+  onResetTimer() {
+    clearInterval(this.timerID);
+    this.minutes = 0;
+    this.seconds = 0;
+    this.tens = 0;
+  }
+
+  format(num: number) {
+    return (num + '').length === 1 ? '0' + num : num + '';
+  }
 }
+
+
+
+
+
+
